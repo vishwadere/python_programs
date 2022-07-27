0ae4f362dfadc400135146bfca94d739afcb94c5
vishwadere <vishwa.dere@gmail.com>
2022-07-27 20:02:03 +0530

    Create min.py

---
+++ min.py
@@ -0,0 +1,9 @@
+a = int(input("enter number : "))
+b = int(input('enter another number : '))
+
+if a<b :
+    print (a,' is smaller')
+elif a==b :
+    print (a,' is equal to ',b)
+else :
+    print (b,' is smaller')

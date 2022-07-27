04aa07d626fca9558351f721a98e0cda85b85ad2
vishwadere <vishwa.dere@gmail.com>
2022-07-27 20:00:24 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,8 @@
+a = int(input("enter number : "))
+b = int(input('enter another number : '))
+if a>b :
+    print (a,' is greater')
+elif a==b :
+    print (a,' is equal to ',b)
+else :
+    print (b,' is greater')
